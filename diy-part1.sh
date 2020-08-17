75646da0d8ec91420092da2aaba79ca9bc44c68c
Pink Floyd <PINK-FL0YD@users.noreply.github.com>
2020-08-17 12:34:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,4 +14,4 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git pink-fl0yd https://github.com/pink-fl0yd/openwrt-package' feeds.conf.default
