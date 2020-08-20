ea7bdd6f5dd0be9e7a2be4d5e4d9d763596b986a
Pink Floyd <k.chenmingtong@gmail.com>
2020-08-20 11:08:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,4 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git package https://github.com/pink-fl0yd/openwrt-package' feeds.conf.default
+sed -i '$a src-git passwall https://github.com/pink-fl0yd/openwrt-package' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+sed -i '$a src-git openclash https://github.com/antonchen/clash-for-openwrt' feeds.conf.default
